d3eaf93fdb1bf538c6c765327def4b9f02ba49ed
Kong Sang Uk <qkqhfp19@naver.com>
2022-11-24 16:27:16 +0900

    ClientEx

---
+++ src/ClientEx.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientEx {
+
+    public static void main(String[] arsg) {
+
+        ServerSocket server = null;
+        Socket socket = null;
+        BufferedReader in = null;
+        BufferedWriter out = null;
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            server = new ServerSocket(9999);
+            System.out.println("연결 대기중....");
+
+            socket = server.accept();
+            System.out.println("연결 되었습니다...");
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            while (true){
+                String inMsg = in.readLine();
+                if (inMsg.equalsIgnoreCase("bey")) {
+                    System.out.println("클라이언트가 나갔습니다.");
+                    break;
+                }
+
+                // 성장 메세지 경우
+                System.out.println("클라이언트 : " + inMsg);
+
+                System.out.print("보내기 >>");
+                String outMag = sc.nextLine();
+                out.write(outMag + "\n");
+                out.flush();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                sc.close();
+                out.close();
+                in.close();
+                socket.close();
+                server.accept();
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
